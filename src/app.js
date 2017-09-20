42fbf36231b582e90185693f67019686d05477ef
Benjamin Dewey <benjamin.m.dewey@gmail.com>
2017-09-20 18:22:07 -0400

    reoganize code into functions

--- src/app.js
+++ src/app.js
@@ -4,17 +4,20 @@ const reversi = require('./reversi.js');
 const readlineSync = require('readline-sync');
 const fs = require('fs');
 
-if (process.argv[2]) { // user may have provided a config file
-  fs.readFile(process.argv[2], 'utf8', (err, data) => {
-    if (!err) { // user provided a config file
-      const config = JSON.parse(data);
-      // generate board using config and begin the game
 
+
+const getConfigFromFile = file => {
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (!err) { // user provided a config file
+      return JSON.parse(data);
     } else {
       // there is an error
+      process.exit(1);
     }
   });
-} else { // ask the user for config info
+};
+
+const getConfigFromUser = () => {
   const boardWidthIsInvalid = (boardWidth) => {
     boardWidth = Number(boardWidth);
     if (isNaN(boardWidth) || boardWidth % 2 !== 0 || boardWidth < 4 || boardWidth > 26) {
@@ -25,16 +28,50 @@ if (process.argv[2]) { // user may have provided a config file
 
   const playerLetterIsInvalid = playerLetter => playerLetter !== 'X' && playerLetter !== 'O';
 
-  console.log('Welcome to Reversi.');
+  console.log('\nWelcome to Reversi.\n');
   let boardWidth;
   while (boardWidthIsInvalid(boardWidth)) {
-    boardWidth = readlineSync.question('How wide should the board be? (even numbers between 4 and 26, inclusive)');
+    boardWidth = readlineSync.question('How wide should the board be? (even numbers between 4 and 26, inclusive)\n> ');
   }
 
   let playerLetter;
   while (playerLetterIsInvalid(playerLetter)) {
-    playerLetter = readlineSync.question('Pick your letter: X (black) or O (white)');
+    playerLetter = readlineSync.question('Pick your letter: X (black) or O (white)\n> ');
   }
-  console.log(`Player is ${playerLetter}`);
+  console.log(`Player is ${playerLetter}\n`);
 
-}
+  const halfWidth = boardWidth / 2;
+  let board = reversi.generateBoard(boardWidth, boardWidth, ' ');
+  board = reversi.setBoardCell(board, 'O', halfWidth, halfWidth);
+  board = reversi.setBoardCell(board, 'O', halfWidth - 1, halfWidth - 1);
+  board = reversi.setBoardCell(board, 'X', halfWidth - 1, halfWidth);
+  board = reversi.setBoardCell(board, 'X', halfWidth, halfWidth - 1);
+
+  return {
+    boardPreset: {
+      playerLetter,
+      board
+    },
+    scriptedMoves: {
+      player: [],
+      computer: []
+    }
+  };
+};
+
+const getConfig = () => {
+  if (process.argv[2]) { // user may have provided a config file
+    getConfigFromFile(process.argv[2]);
+  } else { // ask the user for config info
+    getConfigFromUser();
+  }
+};
+
+
+const playReversi = config => {
+
+};
+
+const runApp = () => {
+  playReversi(getConfig());
+};
